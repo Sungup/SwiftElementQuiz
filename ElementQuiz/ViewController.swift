905299fd8fbcb63b89ac716277ce042e3a17130c
Sungup Moon <sungup@me.com>
2019-02-06 14:23:12 +0900

    Initial Commit

---
+++ ElementQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElementQuiz
+//
+//  Created by 文盛業 on 2019/02/06.
+//  Copyright © 2019 Sungup Priv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+

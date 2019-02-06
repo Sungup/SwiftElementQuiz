c8b83bc555f4ab0de0a5f5c1543945d4cd3d5de0
Sungup Moon <sungup@me.com>
2019-02-06 14:42:38 +0900

    [update] Implement basic functions.

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -12,9 +12,39 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
     // Do any additional setup after loading the view, typically from a nib.
+    currentElementIndex = 0
+
+    updateElement()
   }
 
+  /// Outlets
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var answerLabel: UILabel!
 
+  /// Controls
+  @IBAction func showAnswer(_ sender: UIButton) {
+    answerLabel.text = elementsList[currentElementIndex]
+  }
+
+  @IBAction func gotoNextElement(_ sender: UIButton) {
+    currentElementIndex += 1
+
+    if currentElementIndex >= elementsList.count {
+      currentElementIndex = 0
+    }
+
+    updateElement()
+  }
+
+  /// Properties
+  var currentElementIndex: Int = 0
+  var elementsList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+
+  /// Methods
+  func updateElement() {
+    answerLabel.text = "?"
+    imageView.image = UIImage(named: elementsList[currentElementIndex])
+  }
 }
-
